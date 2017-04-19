26838ccc81280a647831d6f62861c8b8da55bf77
root <root@ubuntu-512mb-nyc3-01.localdomain>
2017-04-19 17:15:50 +0000

    persistent nav bar

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <table style=" width: 100vw; height: 50px; background-color: #fafafb; box-shadow: 0 1px 0 rgba(12,13,14,0.15); border-top: 3px solid #F48024; position:fixed; top:0;" >
 <tr style=" width: 100vw; height: 50px; background-color: #fafafb; box-shadow: 0 1px 0 rgba(12,13,14,0.15); border-top: 3px solid #F48024;">
 <td><img src="/res/stoat.png" style="height: 50px; width:auto;"></td>
-<td style="margin-top:auto; margin-bottom:auto; text-align:center;"><h1>Evolve.js - The collective web standard</h1></td>
+<td style="margin-top:auto; margin-bottom:auto; text-align:center;"><a href="http://evolvejs.tk"><h1 style="color: black">Evolve.js - The collective web standard</h1></a></td>
 <td><h3 style="text-align:center;"><a href='http://evolvejs.tk/editor.html'>Create New Design Spec</a></h3></td>
 <td style="width:50%; text-align: center">
 <form action="#" onsubmit="alert('todo')">
